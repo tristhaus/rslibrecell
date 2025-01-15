0819796b115c4cc7c79b675ded629b748c500568
tristhaus <tristhaus@yahoo.de>
2025-01-15 20:39:37 +0100

    Define basic struct for a game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct Game {
+    id: u16,
+    cells: [i8; 4], // todo: something nullable
+    foundations: [Vec<i8>; 4],
+    columns: [Vec<i8>; 8]
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let game = Game {
+        id: 17,
+        cells: [-1, -1, -1, -1],
+        foundations: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
+        columns: [Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()]
+    };
+
+    println!("{game:?}");
 }
