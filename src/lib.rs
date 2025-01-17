5b1fa3b33e7a4f8c93bc0e4aad2b3a4c343875d1
tristhaus <tristhaus@yahoo.de>
2025-01-17 20:56:38 +0100

    Move test of clone functionality to proper test

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,47 @@ pub mod lib {
         pub foundations: [Vec<i8>; 4],
         pub columns: [Vec<i8>; 8],
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn game_can_be_cloned_yields_independent_instances() {
+            let mut game = Game {
+                id: 17,
+                cells: [None, None, None, None],
+                foundations: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
+                columns: [
+                    Vec::new(),
+                    Vec::new(),
+                    Vec::new(),
+                    Vec::new(),
+                    Vec::new(),
+                    Vec::new(),
+                    Vec::new(),
+                    Vec::new(),
+                ],
+            };
+
+            let clone = game.clone();
+
+            game.id = 11;
+            game.cells[1] = Some(2);
+            game.foundations[2].push(3);
+            game.columns[3].push(4);
+
+            assert_eq!(17, clone.id);
+            assert_eq!(11, game.id);
+
+            assert_eq!(None, clone.cells[1]);
+            assert_eq!(Some(2), game.cells[1]);
+
+            assert!(clone.foundations[2].is_empty());
+            assert_eq!(3, game.foundations[2][0]);
+
+            assert!(clone.columns[3].is_empty());
+            assert_eq!(4, game.columns[3][0]);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use rslibrecell::lib::Game;
-
 fn main() {
     println!("Hello, world!");
-
-    let mut game = Game {
-        id: 17,
-        cells: [None, None, None, None],
-        foundations: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
-        columns: [Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()]
-    };
-
-    let clone = game.clone();
-
-    game.id = 11;
-    game.cells[1] = Some(2);
-    game.foundations[2].push(3);
-
-    println!("game: {game:?}");
-    println!("clone: {clone:?}");
 }
